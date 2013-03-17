c6f618855d69bf5c6defdc76bf5de4edb6f2c677
Alejandro Banzas <alebanzas@outlook.com>
2013-03-16 22:01:53 -0300

    arranco conversor

---
+++ WP/DolarBlue/DolarBlue/Conversor.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace DolarBlue
+{
+    public partial class Conversor : PhoneApplicationPage
+    {
+        public Conversor()
+        {
+            InitializeComponent();
+
+            DataContext = App.ViewModel;
+        }
+    }
+}
