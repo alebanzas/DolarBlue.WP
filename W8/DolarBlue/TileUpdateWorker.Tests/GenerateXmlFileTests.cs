0e51d40b08cd48e4ee3d2b072c8e79f740248e74
Guillermo Bellmann <gbellmann@users.noreply.github.com>
2013-03-18 21:16:39 -0300

    Test del generador

--- W8/DolarBlue/TileUpdateWorker.Tests/GenerateXmlFileTests.cs
+++ W8/DolarBlue/TileUpdateWorker.Tests/GenerateXmlFileTests.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using DolarBlue.BLL;
+using DolarBlue.Services;
 using NUnit.Framework;
 
 namespace TileUpdateWorker.Tests
@@ -8,6 +13,31 @@ namespace TileUpdateWorker.Tests
 		[Test]
 		public void WhenDivisaThenGenerateFile()
 		{
+			var divisa = new DivisaViewModel
+				             {
+					             Nombre = "Dolar Oficial",
+					             Simbolo = "USD",
+					             ValorCompra = "5,02",
+					             ValorVenta = "5,08",
+					             Actualizacion = "18/03/2013",
+					             Variacion = "1,3%",
+				             };
+			var tileXmlGenerator = new TileXmlGenerator();
+
+			const string fileName = "dolaroficial.xml";
+			File.Delete(fileName);
+
+			using (var destination = File.CreateText(fileName))
+			{
+				var wSettings = new XmlWriterSettings { Indent = true, Encoding = Encoding.GetEncoding("ISO-8859-1") };
+				var xw = XmlWriter.Create(destination, wSettings);
+
+				tileXmlGenerator.GenerateXmlFile(xw, divisa);
+
+				//destination.Write(xw);
+				
+				destination.Flush();
+			}
 
 		}
 	}
