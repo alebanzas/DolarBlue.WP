a46dc339faf02a2534cbc64aea994fde0836ca05
Alejandro Banzas <alebanzas@outlook.com>
2014-01-10 22:22:20 -0300

    arranco agent para tile update

---
+++ WP/DolarBlue/DolarBlue.Agent/ScheduledAgent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Shell;
+
+namespace DolarBlue.Agent
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+        private static volatile bool _classInitialized;
+
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        public ScheduledAgent()
+        {
+            if (!_classInitialized)
+            {
+                _classInitialized = true;
+                // Subscribe to the managed exception handler
+                Deployment.Current.Dispatcher.BeginInvoke(delegate
+                {
+                    Application.Current.UnhandledException += ScheduledAgent_UnhandledException;
+                });
+            }
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private void ScheduledAgent_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            if (task is PeriodicTask)
+            {
+                // Execute periodic task actions here.
+                ShellTile tileToFind = ShellTile.ActiveTiles.FirstOrDefault(x => x.NavigationUri.ToString().Contains("TileID=1"));
+                if (tileToFind != null)
+                {
+                    var newTileData = new StandardTileData
+                    {
+                        Title = "Dolar",
+                        BackTitle = DateTime.UtcNow.ToShortTimeString(),
+                        BackContent = "Second content",
+                    };
+                    tileToFind.Update(newTileData);
+                }
+            }
+
+            NotifyComplete();
+        }
+    }
+}
